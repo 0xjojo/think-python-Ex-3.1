0f4348bb9626434e5a950f459138de9d3fe454d4
Gehad Makhlouf <gehadmakhlouf@gmail.com>
2018-06-09 23:45:29 +0300

    think python Ex 3.1

---
+++ Ex-3.1.py
@@ -0,0 +1,9 @@
+print_lyrics()
+
+def print_lyrics():
+    print "I'm a lumber jack, and I'm okay."
+    print "Ï sleep all night and I work all day."
+
+def repeat_lyrics():
+    print_lyrics()
+    print_lyrics()
